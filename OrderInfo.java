b3c82ae6cf2d0c3cbf05214d3f2baa477ce2aae1
Lawrence_Cruz <88377566+lawr-cruz@users.noreply.github.com>
2022-11-09 16:49:19 +0800

    Add files via upload

---
+++ OrderInfo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jaafary
+ */
+public class OrderInfo {
+    String Pax;
+    public String getPax(){
+        return Pax;
+    }
+    public void setPax(String nPax){
+        Pax = nPax;
+    }
+}
